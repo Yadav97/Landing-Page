0c052f3db9be99a5c27470e8a052a26ebd5f7fad
krishna <ky680yadav@gmail.com>
2023-11-01 20:27:54 +0530

    floating to top button added and scroll reveal addedq

--- script.js
+++ script.js
@@ -1,35 +1,54 @@
-    // window.onscroll = function() {myFunction()}; 
-      
-    // var navlist = document.getElementById("navbar"); 
-    // var sticky = navlist.offsetTop; 
-      
-    // /* function to stick the nav bar */
-    // function myFunction() { 
-    //     if (window.pageYOffset >= sticky) { 
-    //         navlist.classList.add("sticky") 
-    //     }  
-    //       else { 
-    //           navlist.classList.remove("sticky"); 
-    //       } 
-    // } 
-
+// sticky navabr code
 
 
     // singup js code
-const openPopupButton = document.getElementById("openPopup");
-const closePopupButton = document.getElementById("closePopup");
-const popup = document.getElementById("signupPopup");
+// const openPopupButton = document.getElementById("openPopup");
+// const closePopupButton = document.getElementById("closePopup");
+// const popup = document.getElementById("signupPopup");
 
-openPopupButton.addEventListener("click", () => {
-    popup.style.display = "block";
-});
+// openPopupButton.addEventListener("click", () => {
+//     popup.style.display = "block";
+// });
 
-closePopupButton.addEventListener("click", () => {
-    popup.style.display = "none";
-});
+// closePopupButton.addEventListener("click", () => {
+//     popup.style.display = "none";
+// });
 
-window.addEventListener("click", (event) => {
-    if (event.target == popup) {
-        popup.style.display = "none";
-    }
-});
+// window.addEventListener("click", (event) => {
+//     if (event.target == popup) {
+//         popup.style.display = "none";
+//     }
+// });
+
+
+
+
+// Get the button:
+let mybutton = document.getElementById("topBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {
+    scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+
+  document.documentElement.scrollTop = 0; 
+} 
+
+// ScrollReveal().reveal('.reveal-nav');
+ScrollReveal().reveal('.reveal-random', { delay: 1000 });
+ScrollReveal().reveal('.reveal-quote', { delay: 1000 });
+ScrollReveal().reveal('.reveal-signup', { delay: 800 });
+ScrollReveal().reveal('.reveal-footer');
+
+
